5ce4d02c438d2337bfc39b885f9ba297fae3e341
Anirudh Bali <balianirudh@gmail.com>
2020-06-09 23:37:45 -0400

    Update pathDetection.py

--- Software/videoProcessing1/pathDetection.py
+++ Software/videoProcessing1/pathDetection.py
@@ -1,3 +1,13 @@
+# The purpose of this program was to identify a marked path as the desired path of the drone. After the video is taken by a downward 
+# facing USB camera, the video is processed by this program using OpenCV in python. The program not only identifies the desired path but 
+# it also calculates the angle error between the desired and current path of the drone. This angle is used in the controller of the drone 
+# to keep it moving along the desired path.
+
+# Credit for the path detection development goes to kemfic on GitHub. His post about "Simple Lane Detection" on hackster.io inspired the 
+# path detection aspect of this project. I have used his path detection software as the foundation of my path detection program in this 
+# project. If you are interested in an indepth article on how kemfic developed his simple lane detection software please refer to his 
+# article on hackster.io (https://www.hackster.io/kemfic/simple-lane-detection-c3db2f).
+
 #! /usr/bin/env python
 
 from __future__ import division
